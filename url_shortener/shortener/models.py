59b3158fcf90535c0f8889a1af57e82cd0cdd3d0
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-04-30 15:41:11 +0300

    Added model Url

--- url_shortener/shortener/models.py
+++ url_shortener/shortener/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Url(models.Model):
+    original_link = models.URLField()
+    shortened_link = models.CharField(max_length=100, blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.pk}: {self.original_link} - {self.shortened_link}"
+
+    class Meta:
+        verbose_name = "Url"
+        verbose_name_plural = "Urls"
